6ed461f952f4d7a7f96929890b42029ffddf394a
EdgarMosima <nkalebetjamosima@gmail.com>
2022-10-08 22:17:07 +0200

    First commit

---
+++ src/main/java/GROUP1/API/App.java
@@ -0,0 +1,24 @@
+package GROUP1.API;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
